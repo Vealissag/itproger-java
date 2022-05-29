9c18fdf72f860946bae526b462b27087d2199435
Vealissag <sag_@ukr.net>
2022-05-29 12:48:30 +0300

    Init 3

--- src/com/itproger/Test1.java
+++ src/com/itproger/Test1.java
@@ -1,4 +1,21 @@
 package com.itproger;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Test1 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+    private static int sum (int number1, int number2){
+        int res = number1 + number2;
+        return res;
+    }
 }
